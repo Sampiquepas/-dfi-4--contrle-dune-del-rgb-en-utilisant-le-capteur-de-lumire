3bd825e817ddc020a959857424167ab1aaf06e8b
Sampiquepas <92890152+Sampiquepas@users.noreply.github.com>
2021-11-12 13:56:11 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let intensité = 0
 function bleu () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
 function rouge () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
 }
 function vert () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
 }
 basic.forever(function () {
     intensité = pins.analogReadPin(AnalogPin.P0)
