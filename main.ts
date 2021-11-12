f90d67a5b8112897de9150b057c9e2165ebeff81
Sampiquepas <92890152+Sampiquepas@users.noreply.github.com>
2021-11-12 14:12:58 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 let intensité = 0
 function bleu () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function VIOLET () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function BLANC () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 1)
@@ -7,12 +17,12 @@ function bleu () {
 function rouge () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
 function vert () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
 basic.forever(function () {
     intensité = pins.analogReadPin(AnalogPin.P0)
@@ -20,7 +30,11 @@ basic.forever(function () {
         vert()
     } else if (intensité < 700 && intensité >= 300) {
         bleu()
-    } else {
+    } else if (intensité < 300 && intensité >= 200) {
         rouge()
+    } else if (intensité < 200 && intensité >= 150) {
+        BLANC()
+    } else {
+        VIOLET()
     }
 })
